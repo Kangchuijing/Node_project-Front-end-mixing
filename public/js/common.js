6c154b0792b2e7f1d3456547d36a0c3626cb2c62
Kangchuijing <1806993519@qq.com>
2018-11-27 12:27:33 +0800

    初步写完页面

---
+++ public/js/common.js
@@ -0,0 +1,3 @@
+// header 顶部js代码
+var $ = require('jquery');
+console.log($);
---
+++ routes/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/index.html', function (req, res, next) {
+  var query = req.query;
+  if (query.user == 'kang' && query.password == '123456') {
+    res.render('index', { title: '后台管理系统' });
+  }
+  else {
+    res.render('login', { error: '用户名或密码错误' });
+  }
+});
+router.get('/login.html', function (req, res, next) {
+  res.render('login', { error: '' });
+});
+router.get('/regesit.html', function (req, res, next) {
+  res.render('regesit', { error: "" });
+});
+router.get('/user', function (req, res) {
+  res.render('index', { title: '后台管理-用户管理' })
+});
+router.get('/pinpai', function (req, res) {
+  res.render('index', { title: '后台管理-品牌管理' })
+});
+router.get('/phone', function (req, res) {
+  res.render('index', { title: '后台管理-手机管理' })
+});
+module.exports = router;
